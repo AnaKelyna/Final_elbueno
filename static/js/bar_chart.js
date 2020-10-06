332200521afb3cabeaa5bb8ea36f75d6450d05ca
AnaKelyna <64177211+AnaKelyna@users.noreply.github.com>
2020-10-05 23:38:36 -0500

    Create bar_chart.js

---
+++ static/js/bar_chart.js
@@ -0,0 +1,48 @@
+const url3 = "https:5000//127.0.0.1/graph/product";
+
+d3.json(url3).then(function(json) {
+
+    console.log(json);
+
+    var products = [];
+    var num_prod = [];
+
+    json.forEach(function(data) {
+        products.push(data.product_name);
+        num_prod.push(data.Total_product);
+    });
+
+    console.log(products)
+
+    var x = products
+    var y = num_prod
+    console.log(x)
+    console.log(y)
+
+    // Create the Trace
+    var trace1 = {
+        x: x,
+        y: y,
+        type: "bar",
+    };
+    // Create the data array for the plot
+    var data1 = [trace1];
+
+    // Define the plot layout
+    var layout = {
+        title: "Top 10 best sellers",
+        height: 600,
+        width: 400,
+        xaxis: {
+            title: "Sample values",
+        },
+        yaxis: {
+            title: "orders",
+            type: 'integer'
+        }
+    };
+
+    // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("bar1", data1, layout);
+
+});
